8b3310afd16ff6f5081d9f82a9ba325fd4b0e527
stemolti <s.agbodan@eggonemail.com>
2025-04-02 16:47:41 +0200

    Fixed CRLF

---
+++ src/api/projects/dto/update-project.dto.ts
@@ -0,0 +1,15 @@
+import { IsInt, IsOptional, IsString } from 'class-validator';
+
+export class UpdateProjectDto {
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsInt()
+  @IsOptional()
+  updatedAt?: Date;
+}
