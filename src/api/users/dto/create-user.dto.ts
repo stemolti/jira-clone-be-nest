08bd3d12735adc253282329ea1963da9520fd7f1
stemolti <s.agbodan@eggonemail.com>
2025-04-02 16:51:20 +0200

    Lint Formating update

--- src/api/users/dto/create-user.dto.ts
+++ src/api/users/dto/create-user.dto.ts
@@ -1,7 +1,6 @@
-import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDTO {
-
   @IsEmail()
   @IsNotEmpty()
   email: string;
@@ -10,9 +9,7 @@ export class CreateUserDTO {
   @IsNotEmpty()
   name: string;
 
-  
   createdAt: Date;
-
 
   updatedAt: Date;
 }
