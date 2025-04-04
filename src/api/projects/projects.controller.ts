e2c9eee0bbc84831ab820d094d9e4e20ce70edc7
stemolti <s.agbodan@eggonemail.com>
2025-04-04 15:59:45 +0200

    Added Projects controller

--- src/api/projects/projects.controller.ts
+++ src/api/projects/projects.controller.ts
@@ -1,9 +1,14 @@
 import { CacheInterceptor } from '@nestjs/cache-manager';
-import { Controller, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 
 @Controller('projects')
 @UseInterceptors(CacheInterceptor)
 export class ProjectsController {
   constructor(private projectsService: ProjectsService) {}
+
+  @Get()
+  async getProjects() {
+    return this.projectsService.getAllProjects();
+  }
 }
